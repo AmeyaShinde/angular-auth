defcb34ffccbd83f61dac6f2579ecf70fadec592
Ameya Shinde <ameyashinde123@outlook.com>
2022-12-12 01:49:25 +0530

    Login form validations.

--- src/AngularAuthUI/src/app/app.module.ts
+++ src/AngularAuthUI/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +8,17 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        SignupComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/AngularAuthUI/src/app/components/login/login.component.ts
+++ src/AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -10,16 +11,44 @@ export class LoginComponent implements OnInit {
     type: string = "password";
     isText: boolean = false;
     eyeIcon: string = "fa-eye-slash"
+    loginForm!: FormGroup;
 
-    constructor() { }
+    constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
+        this.loginForm = this.fb.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
     }
 
     hideshowPass() {
         this.isText = !this.isText;
         this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
         this.isText ? this.type = "text" : this.type = "password";
+    }
+
+    onSubmit() {
+        if (this.loginForm.valid) {
+            console.log(this.loginForm.value);
+            // Send the obj to database
+        } else {
+            console.log("Form is not valid.");
+            // throw the error using toaster and with the required fields
+            this.validateAllFormFields(this.loginForm);
+            alert("Your form is invalid.");
+        }
+    }
+
+    private validateAllFormFields(formGroup: FormGroup) {
+        Object.keys(formGroup.controls).forEach(field => {
+            const control = formGroup.get(field);
+            if (control instanceof FormControl) {
+                control?.markAsDirty({ onlySelf: true });
+            } else if (control instanceof FormGroup) {
+                this.validateAllFormFields(control);
+            }
+        })
     }
 
 }
