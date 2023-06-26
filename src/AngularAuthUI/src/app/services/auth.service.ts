ec1791f8b1f76027c4265fc012cd473b2441e281
Ameya Shinde <ameya.shinde@kwglobal.com>
2023-06-27 01:37:06 +0530

    Save the Hashed Password.

--- src/AngularAuthUI/src/app/services/auth.service.ts
+++ src/AngularAuthUI/src/app/services/auth.service.ts
@@ -6,15 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-    private baseUrl: string = "https://localhost:7276/api/User/";
+    private baseUrl: string = "https://localhost:7276/api/User";
 
     constructor(private http: HttpClient) { }
 
     signUp(userObj: any) {
-        return this.http.post<any>(`${this.baseUrl}register`, userObj);
+        return this.http.post<any>(`${ this.baseUrl }/register`, userObj);
     }
 
     login(loginObj: any) {
-        return this.http.post<any>(`${this.baseUrl}authenticate`, loginObj);
+        return this.http.post<any>(`${ this.baseUrl }/authenticate`, loginObj);
     }
 }
