61f657879602c731a8165d37b306baf82c20bd33
Ameya Shinde <ameya.shinde@kwglobal.com>
2023-06-30 00:48:33 +0530

    Added Logout button.

--- src/AngularAuthUI/src/app/components/dashboard/dashboard.component.ts
+++ src/AngularAuthUI/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
+    public users: any = [];
+
+    constructor(private auth: AuthService, private api: ApiService) { }
+
+    ngOnInit() {
+        this.api.getUsers().subscribe(res => {
+            this.users = res;
+        });
+    }
+
+    logout() {
+        this.auth.logout();
+    }
 }
--- src/AngularAuthUI/src/app/services/auth.service.ts
+++ src/AngularAuthUI/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
@@ -8,7 +9,7 @@ export class AuthService {
 
     private baseUrl: string = "https://localhost:7276/api/User";
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     signUp(userObj: any) {
         return this.http.post<any>(`${ this.baseUrl }/register`, userObj);
@@ -16,6 +17,11 @@ export class AuthService {
 
     login(loginObj: any) {
         return this.http.post<any>(`${ this.baseUrl }/authenticate`, loginObj);
+    }
+
+    logout() {
+        localStorage.clear();
+        this.router.navigate(['login']);
     }
 
     storeToken(tokenValue: string) {
