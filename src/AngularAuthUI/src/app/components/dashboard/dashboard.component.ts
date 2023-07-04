dc28ad0372bb6556a056bca811c2f8893a6b6b55
Ameya Shinde <ameya.shinde@kwglobal.com>
2023-07-04 19:56:02 +0530

    Used the JWT payload from token

--- src/AngularAuthUI/src/app/components/dashboard/dashboard.component.ts
+++ src/AngularAuthUI/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserStoreService } from 'src/app/services/user-store.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -10,12 +11,22 @@ import { AuthService } from 'src/app/services/auth.service';
 export class DashboardComponent {
 
     public users: any = [];
+    public fullName: string = "";
 
-    constructor(private auth: AuthService, private api: ApiService) { }
+    constructor(
+        private auth: AuthService,
+        private api: ApiService,
+        private userStore: UserStoreService
+    ) { }
 
     ngOnInit() {
         this.api.getUsers().subscribe(res => {
             this.users = res;
+        });
+
+        this.userStore.getFullNameFromStore().subscribe(val => {
+            let fullNameFromToken = this.auth.getFullNameFromToken();
+            this.fullName = val || fullNameFromToken;
         });
     }
 
